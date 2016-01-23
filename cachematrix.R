0a56206317220a0595e1c5909f35b53443bb3304
Narisu Narisu(NHGRI) <narisu@HG-01931194-LM9.local>
2016-01-23 16:32:33 -0500

    Finished assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The following functions calculate the inverse of a matrix and saves it
+# to the cache such that the next time the user attempts to calculate the
+# matrix inverse, the previously saved value is returned instead of
+# repeating the calculation.
 
-## Write a short comment describing this function
+# This function creates a special "matrix" object, which is really a list 
+# containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of the inverse
+# 4. get the value of the inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCasheMatrix<-function(x = matrix()) {
+    inv<-NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <-function(inverse) inv <<- inverse 
+    getinverse <-function() inv 
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
 }
 
+# The following function calculates the inverse of the special "matrix" created
+# with the above function. However, it first checks to see if the inverse
+# has already been caclulated. If so, it 'get's the inverse from the cache
+# and skips the computation. Otherwise, it calculates the matrix inverse
+# and sets the value of the inverse in the cache via the 'setinverse' function.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cashSolve <- function(x, ...) {
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv<-solve(data,...)
+    x$setinverse(inv)
+    inv 
 }
